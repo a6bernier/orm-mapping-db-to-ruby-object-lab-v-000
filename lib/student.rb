5731d19379e16bec0277c670e960214b434ab155
Andrew Bernier <a6bernier@gmail.com>
2018-11-06 20:11:10 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -60,6 +60,17 @@ class Student
     end
   end
 
+  def self.first_X_students_in_grade_10(number)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 10
+    SQL
+
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+  end
+
   def self.create_table
     sql = <<-SQL
     CREATE TABLE IF NOT EXISTS students (
